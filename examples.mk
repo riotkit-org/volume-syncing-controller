bd7ef068139f049605fc9ed0768b1fee0e96bcd2
Maya Baya <maya@example.org>
2022-05-31 21:11:00 +0200

    fix: Not executing tests on CI - `make: .build/volume-syncer: Command not found`

--- examples.mk
+++ examples.mk
@@ -6,7 +6,7 @@ test: test_sync_without_encryption test_sync_using_envs test_remote_to_local
 #
 .PHONY: test_sync_without_encryption
 test_sync_without_encryption:
-	.build/volume-syncer sync-to-remote -d testbucket -p 'type=s3' -p 'provider=Minio' -p 'access_key_id=AKIAIOSFODNN7EXAMPLE' -p 'secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY' -p 'endpoint = http://localhost:9000' -p 'acl = private'
+	.build/volume-syncing-operator sync-to-remote -d testbucket -p 'type=s3' -p 'provider=Minio' -p 'access_key_id=AKIAIOSFODNN7EXAMPLE' -p 'secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY' -p 'endpoint = http://localhost:9000' -p 'acl = private'
 
 #
 # Environment variables are more handy when using Docker, docker-compose and Kubernetes
@@ -19,7 +19,7 @@ test_sync_using_envs:
 	export REMOTE_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY; \
 	export REMOTE_ENDPOINT=http://localhost:9000; \
 	export REMOTE_ACL=private; \
-	.build/volume-syncer sync-to-remote -d testbucket
+	.build/volume-syncing-operator sync-to-remote -d testbucket
 
 .PHONY: test_sync_every_1_minute
 test_sync_every_1_minute:
@@ -29,11 +29,11 @@ test_sync_every_1_minute:
 	export REMOTE_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY; \
 	export REMOTE_ENDPOINT=http://localhost:9000; \
 	export REMOTE_ACL=private; \
-	.build/volume-syncer sync-to-remote -d testbucket --schedule "@every 1m"
+	.build/volume-syncing-operator sync-to-remote -d testbucket --schedule "@every 1m"
 
 test_remote_to_local:
 	# upload
-	.build/volume-syncer sync-to-remote -d testbucket -s ./pkg -p 'type=s3' -p 'provider=Minio' -p 'access_key_id=AKIAIOSFODNN7EXAMPLE' -p 'secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY' -p 'endpoint = http://localhost:9000' -p 'acl = private'
+	.build/volume-syncing-operator sync-to-remote -d testbucket -s ./pkg -p 'type=s3' -p 'provider=Minio' -p 'access_key_id=AKIAIOSFODNN7EXAMPLE' -p 'secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY' -p 'endpoint = http://localhost:9000' -p 'acl = private'
 
 	# then download into different directory
-	.build/volume-syncer remote-to-local-sync -v -s testbucket -d ./.build/testing-restore -p 'type=s3' -p 'provider=Minio' -p 'access_key_id=AKIAIOSFODNN7EXAMPLE' -p 'secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY' -p 'endpoint = http://localhost:9000' -p 'acl = private'
+	.build/volume-syncing-operator remote-to-local-sync -v -s testbucket -d ./.build/testing-restore -p 'type=s3' -p 'provider=Minio' -p 'access_key_id=AKIAIOSFODNN7EXAMPLE' -p 'secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY' -p 'endpoint = http://localhost:9000' -p 'acl = private'
